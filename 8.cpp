16b116c4277bcbfb00bff6cb7e983ec2bff50059
chiragsoni16 <chiragsoni8058631695@gmail.com>
2024-03-20 01:56:15 +0530

    to find the number is even or odd

--- 8.cpp
+++ 8.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std; 
 int main() {
-    cout<<"Enter the number to check weather it is even or odd";
+    cout<<"Enter the number to check weather it is even or odd"<<endl;
     int n;
     cin>>n;
     if(n%2==0){
-        cout<<"The number is Even";
+        cout<<"The number is Even"<<endl;
     }
     else{
-        cout<<"The number is Odd";
+        cout<<"The number is Odd"<<endl;
     }
     return 0;
 }
